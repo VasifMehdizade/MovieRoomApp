001077d0cf4b83039fc962600ca9b40f8ea5a442
VasifMehdizade <109298511+VasifMehdizade@users.noreply.github.com>
2022-09-24 17:25:26 +0400

    project created

---
+++ MovieRoomApp/MovieRoomApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieRoomApp
+//
+//  Created by Vasif Mehdi on 24.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
