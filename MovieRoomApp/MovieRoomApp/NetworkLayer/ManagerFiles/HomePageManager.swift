6c0dcb37eff2c4fcc156f4e810a659581b9feabb
VasifMehdizade <109298511+VasifMehdizade@users.noreply.github.com>
2022-09-24 17:48:49 +0400

    other controllers added

---
+++ MovieRoomApp/MovieRoomApp/NetworkLayer/ManagerFiles/HomePageManager.swift
@@ -0,0 +1,27 @@
+//
+//  HomePageManager.swift
+//  MovieRoomApp
+//
+//  Created by Vasif Mehdi on 24.09.22.
+//
+
+import Foundation
+
+class HomePageManager {
+    static let shared = HomePageManager()
+    
+    func getMovies (complete: @escaping ((Movies?, String?)->())) {
+        let url = "https://api.themoviedb.org/3/movie/now_playing?api_key=d8255fdb07be4ce132643e5e155bf54b"
+        NetworkManager.shared.request(type: Movies.self,
+                                      url: url,
+                                      method: .get) {
+            response in
+            switch response {
+            case .success(let model) :
+                complete(model, nil)
+            case .failure(let error) :
+                complete(nil, error.localizedDescription)
+            }
+        }
+    }
+}
---
+++ MovieRoomApp/MovieRoomApp/Scene/HomePage/View/HomePageController.swift
@@ -0,0 +1,18 @@
+//
+//  HomePageController.swift
+//  MovieRoomApp
+//
+//  Created by Vasif Mehdi on 24.09.22.
+//
+
+import UIKit
+
+class HomePageController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
+
+    }
+  
+}
