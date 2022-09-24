a1049e495cf02e42234f9ac31a41df4991e62934
VasifMehdizade <109298511+VasifMehdizade@users.noreply.github.com>
2022-09-24 18:28:58 +0400

    headerCollectionReusableView created

---
+++ MovieRoomApp/MovieRoomApp/Scene/HomePage/View/Cell/MoviesCell.swift
@@ -0,0 +1,18 @@
+//
+//  MoviesCell.swift
+//  MovieRoomApp
+//
+//  Created by Vasif Mehdi on 24.09.22.
+//
+
+import UIKit
+
+class MoviesCell: UICollectionViewCell {
+    @IBOutlet private weak var moviesImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ MovieRoomApp/MovieRoomApp/Scene/HomePage/View/Controllers/HeaderCollectionReusableView.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderCollectionReusableView.swift
+//  MovieRoomApp
+//
+//  Created by Vasif Mehdi on 24.09.22.
+//
+
+import UIKit
+
+class HeaderCollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
